841d05a329915d93585217f622832bb40d6c24c7
NeerajKaplas <=>
2015-02-03 15:43:01 +0530

    #1: Disable all links except sign-in button on login page.

--- test/layout/client/app/auth/module.js
+++ test/layout/client/app/auth/module.js
@@ -14,29 +14,7 @@ define([
 
 
     module.config(function ($stateProvider, $couchPotatoProvider) {
-        $stateProvider.state('realLogin', {
-            url: '/real-login',
-
-            views: {
-                root: {
-                    templateUrl: "app/auth/login/login.html",
-                    controller: 'LoginCtrl',
-                    resolve: {
-                        deps: $couchPotatoProvider.resolveDependencies([
-                            'auth/models/User',
-                            'auth/login/LoginCtrl'
-                        ])
-                    }
-                }
-            },
-            data: {
-                title: 'Login',
-                rootId: 'extra-page'
-            }
-
-        })
-
-        .state('login', {
+        $stateProvider.state('login', {
             url: '/login',
             views: {
                 root: {
@@ -49,57 +27,6 @@ define([
                 htmlId: 'extr-page'
             }
         })
-
-        .state('register', {
-            url: '/register',
-            views: {
-                root: {
-                    templateUrl: 'app/auth/views/register.html'
-                }
-            },
-            data: {
-                title: 'Register',
-                htmlId: 'extr-page'
-            },
-            resolve: {
-                deps: $couchPotatoProvider.resolveDependencies([
-                    'modules/forms/directives/validate/smartValidateForm'
-                ])
-            }
-        })
-
-        .state('forgotPassword', {
-            url: '/forgot-password',
-            views: {
-                root: {
-                    templateUrl: 'app/auth/views/forgot-password.html'
-                }
-            },
-            data: {
-                title: 'Forgot Password',
-                htmlId: 'extr-page'
-            },
-            resolve: {
-                deps: $couchPotatoProvider.resolveDependencies([
-                    'modules/forms/directives/validate/smartValidateForm'
-                ])
-            }
-        })
-
-        .state('lock', {
-            url: '/lock',
-            views: {
-                root: {
-                    templateUrl: 'app/auth/views/lock.html'
-                }
-            },
-            data: {
-                title: 'Locked Screen',
-                htmlId: 'lock-page'
-            }
-        })
-
-
     });
 
     module.run(function($couchPotato){
